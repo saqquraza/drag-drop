b3672375ce322155522ec8d38ec676f66b2a0cb7
saqquraza <saqlainaman0@gmail.com>
2023-02-16 17:29:18 +0530

    changes in css

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,195 +1,137 @@
-// import React, { useState } from "react";
-// import img3 from '../Images/download121.jpg';
-// import img2 from '../Images/download23.jpg';
-// import img1 from '../Images/tree.jpg'
+import React, { useState } from "react";
+import img3 from "../Images/download121.jpg";
+import img2 from "../Images/download23.jpg";
+import img1 from "../Images/tree.jpg";
+import './Image.css'
 
-// const Image = ({ src }) => {
-
-//   const [images, setImages] = useState([
-//     {id: 1, src: img1},
-//     {id: 2, src: img2 },
-//     {id: 3, src: img3},
-//   ]);
-//   const [droppedItems, setDroppedItems] = useState([]);
-//   const [resizing, setResizing] = useState(false);
-//   const [startX, setStartX] = useState(0);
-//   const [startY, setStartY] = useState(0);
-//   const [corner, setCorner] = useState("");
-
-//   const handleDrop = (e) => {
-//     e.preventDefault();
-//     setDroppedItems([...droppedItems, e.dataTransfer.getData("text")]);
-//     console.log(e.dataTransfer.getData("text"));
-//   };
-
-//   const handleDragOver = (e) => {
-//     e.preventDefault();
-//   };
-
-//   const handleDragStart = (e) => {
-//     e.dataTransfer.setData("text", e.target.id);
-//     console.log(e.dataTransfer.getData("text"));
-//   };
-
-//   const handleDrag = (e) => {
-//     e.preventDefault();
-//   };
-
-
-//   const handleMouseDown = (e) => {
-//     const image = e.target;
-//     const imageRect = image.getBoundingClientRect();
-//     const imageX = imageRect.left + window.pageXOffset;
-//     const imageY = imageRect.top + window.pageYOffset;
-//     const mouseX = e.clientX;
-//     const mouseY = e.clientY;
-//     if (mouseX < imageX + 20 && mouseY < imageY + 20) {
-//         setCorner("top-left");
-//     } else if (mouseX > imageX + imageRect.width - 20 && mouseY < imageY + 20) {
-//         setCorner("top-right");
-//     } else if (mouseX < imageX + 20 && mouseY > imageY + imageRect.height - 20) {
-//         setCorner("bottom-left");
-//     } else if (mouseX > imageX + imageRect.width - 20 && mouseY > imageY + imageRect.height - 20) {
-//         setCorner("bottom-right");
-//     } else {
-//         return;
-//     }
-//     setResizing(true);
-//     setStartX(e.clientX);
-//     setStartY(e.clientY);
-// };
-
-// const handleMouseUp = () => {
-//     setResizing(false);
-// };
-
-// const handleMouseMove = (e) => {
-//     if (!resizing) {
-//         return;
-//     }
-//     const image = e.target;
-//     const diffX = e.clientX - startX;
-//     const diffY = e.clientY - startY;
-//     switch (corner) {
-//         case "top-left":
-//             image.style.width = `${image.offsetWidth - diffX}px`;
-//             image.style.height = `${image.offsetHeight - diffY}px`;
-//             image.style.left = `${image.offsetLeft + diffX}px`;
-//             image.style.top = `${image.offsetTop + diffY}px`;
-//             break;
-//         case "top-right":
-//             image.style.width = `${image.offsetWidth + diffX}px`;
-//             image.style.height = `${image.offsetHeight - diffY}px`;
-//             image.style.top = `${image.offsetTop + diffY}px`;
-//             break;
-//         case "bottom-left":
-//             image.style.width = `${image.offsetWidth - diffX}px`;
-//             image.style.height = `${image.offsetHeight + diffY}px`;
-//             image.style.left = `${image.offsetLeft + diffX}px`;
-//             break;
-//             case "bottom-right":
-//               image.style.width =` ${image.offsetWidth + diffX}px`;
-//               image.style.height = `${image.offsetHeight + diffY}px`;
-//               break;
-//               default:
-//               break;
-//               }
-//               setStartX(e.clientX);
-//               setStartY(e.clientY);
-//               };
-
-
-//   return (
-//     <>
-//       {images.map((image , index) => (
-//         <img
-//           key={index}
-//           id={image.id}
-//           src={image.src}
-//           draggable={true}
-//           onDragStart={handleDragStart}
-//           onDrag={handleDrag}
-//           style={{
-//             //   position: "absolute",
-//             //   left: position.x + "px",
-//             //   top: position.y + "px",
-//             margin: "10px",
-//           }}
-//         />
-//       ))}
-//       <div
-//         onDrop={handleDrop}
-//         onDragOver={handleDragOver}
-//         onMouseDown={handleMouseDown}
-//            onMouseUp={handleMouseUp}
-//           onMouseMove={handleMouseMove}
-//         style={{
-//           width: "800px",
-//           height: "400px",
-//           border: "1px solid black",
-//           margin:"10px"
-//         }}
-//       >
-//         {droppedItems.map((droppedItem) => {
-//           const image = images.find((image) => {
-//             console.log("image.id:", image.id);
-//             return image.id==droppedItem;
-//         });
-//           if (!image) {
-//             console.error(`Image with id ${droppedItem} not found`);
-//             return null;
-//           }
-//           return <img 
-//           key={droppedItem}
-//            src={image.src} 
-//            style={{margin:'5px'}}
-           
-//            />;
-//         })}
-//       </div>
-//     </>
-//   );
-// };
-// export default Image;
-
-
-import React, { useState } from 'react';
-import img1 from '../Images/tree.jpg'
-
-function Image() {
-  const [size, setSize] = useState({ width: 200, height: 200 });
+const Image = () => {
+  const [images, setImages] = useState([
+    { id: 1, src: img1 },
+    { id: 2, src: img2 },
+    { id: 3, src: img3 },
+  ]);
+  const [droppedItems, setDroppedItems] = useState([]);
+  const [size, setSize] = useState({ width: 100, height: 100 });
   const [aspectRatio, setAspectRatio] = useState(size.width / size.height);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const[imgId,setImgId]=useState(0);
+  // const [selectedItem, setSelectedItem] = useState(null);
 
-  const handleDrag = (event) => {
+  const handleDragImage = (event) => {
+    // if (selectedItem === null) return;
     const startSize = size;
     const startPosition = { x: event.pageX, y: event.pageY };
-    
+
     function onMouseMove(event) {
       const newWidth = startSize.width - startPosition.x + event.pageX;
-      const newHeight = newWidth / aspectRatio;
+      const newHeight = newWidth ;
 
       setSize({ width: newWidth, height: newHeight });
     }
     function onMouseUp() {
-      document.body.removeEventListener("mousemove", onMouseMove);
-      document.body.removeEventListener("mouseup", onMouseUp);
+      myDiv.removeEventListener("mousemove", onMouseMove);
+      myDiv.removeEventListener("mouseup", onMouseUp);
+      setSelectedImage(null);
+      // setSelectedItem(null);
     }
+const myDiv=document.querySelector("#my-div");
+myDiv.addEventListener("mousemove", onMouseMove);
+myDiv.addEventListener("mouseup", onMouseUp, { once: true });
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setDroppedItems([...droppedItems, e.dataTransfer.getData("text")]);
     
-    document.body.addEventListener("mousemove", onMouseMove);
-    document.body.addEventListener("mouseup", onMouseUp ,{ once: true });
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDragStart = (e) => {
+    e.dataTransfer.setData("text", e.target.id);
+   
+  };
+
+  const handleDrag = (e) => {
+    e.preventDefault();
+  };
+
+  const handleImageClick = (event, imageId) => {
+    setSelectedImage(imageId);
+    event.stopPropagation();
   };
 
   return (
-    <div style={{width:'100vw',height:'65vw', backgroundColor:"black",display:'flex',justifyContent:'center',alignItems:'center'}}>
-    <div style={{ width: size.width, height: size.height, position: 'relative' , margin:'20px',backgroundColor:'red'}} >
-      <img src={img1} alt="resizeable" style={{ width: '100%', height: '100%' }} />
-      <button type="button" style={{ position: 'absolute', bottom: 0, right: 0 }}  onMouseDown={handleDrag}>Resize</button>
-      <button type="button" style={{ position: 'absolute', top: 0, right: 0 }}  onMouseDown={handleDrag}>Resize</button>
-      <button type="button" style={{ position: 'absolute', bottom: 0, left: 0 }}  onMouseDown={handleDrag}>Resize</button>
-      <button type="button" style={{ position: 'absolute', top: 0, left: 0 }}  onMouseDown={handleDrag}>Resize</button>
-    </div>
-    </div>
+    <>
+      <div style={{ display: "flex" }}>
+        <div className="image-cont">
+          {images.map((image, index) => (
+            <img
+              key={index}
+              id={image.id}
+              src={image.src}
+              draggable={true}
+              onDragStart={handleDragStart}
+              onDrag={handleDrag}
+              style={{ margin: "10px" }}
+            />
+          ))}
+        </div>
+        <div id="my-div"
+           onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          draggable={false}
+        >
+          {droppedItems.map((droppedItem, index) => {
+            const image = images.find((image) => {
+            
+              return image.id == droppedItem;
+            });
+            const isSelected = selectedImage === image.id;
+           
+            // const isSelectedItem = index === selectedItem;
+           
+            return (
+              <>
+                <div className="div-cont-img"
+                draggable={false}
+                  style={{
+                    width:  size.width,
+                    height: size.height,
+                   
+                  }}
+                >
+                  <img
+                      draggable={false}
+                      onClick={(event) => handleImageClick(event, image.id)}
+                      src={image.src}
+                      style={{
+                      width: "100%",
+                      height: "100%",
+                      border: isSelected ? "1px solid blue" : "none",
+                    }}
+                  />
+                  <div className="top-left-div"
+                    onMouseDown={handleDragImage}
+                  ></div>
+                  <div className="bottom-right-div"
+                    onMouseDown={handleDragImage}
+                  ></div>
+                  <div className="top-right-div"
+                    onMouseDown={handleDragImage}
+                  ></div>
+                  <div className="bottom-left-div"
+                    onMouseDown={handleDragImage}
+                  ></div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
-}
-
+};
 export default Image;
